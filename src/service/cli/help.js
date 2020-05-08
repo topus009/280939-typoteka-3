e54bf33636d5d2a9de1d73fb285dae0fab52b153
topus009 <sj_89@mail.ru>
2020-05-09 01:49:41 +0300

    add help script

---
+++ src/service/cli/help.js
@@ -0,0 +1,20 @@
+'use strict';
+const ch = require(`chalk`);
+
+const message = `
+Программа запускает ${ch.red(`http-сервер`)} и формирует файл с данными для API.
+
+    Гайд:
+    ${ch.green(`service.js`)} <${ch.yellow(`command`)}>
+    Команды:
+    ${ch.green(`--version`)}:            выводит номер версии
+    ${ch.green(`--help`)}:               печатает этот текст
+    ${ch.green(`--generate`)} <${ch.yellow(`count`)}>    формирует файл ${ch.yellow(`mocks.json`)}
+`;
+
+module.exports = {
+  name: `--help`,
+  run() {
+    console.log(message);
+  }
+};
