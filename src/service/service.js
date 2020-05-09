a817b1872e58226d6daf925f5d4ebc00a8ab2ccc
topus009 <sj_89@mail.ru>
2020-05-09 17:50:28 +0300

    add service and utils

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {cli} = require(`./cli`);
+const {exit} = require(`../utils/utils`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+} = require(`../config/constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !cli[userCommand]) {
+  cli[DEFAULT_COMMAND].run();
+  exit(`success`);
+}
+
+cli[userCommand].run(userArguments.slice(1));
