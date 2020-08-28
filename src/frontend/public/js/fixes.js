ca30fabf6f7d4b98286dd44032cf4fb744f8f2a6
topus009 <sj_89@mail.ru>
2020-08-29 02:32:26 +0300

    hotfix 2

--- src/frontend/public/js/fixes.js
+++ src/frontend/public/js/fixes.js
@@ -1,42 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
+'use strict';
+
+document.addEventListener(`DOMContentLoaded`, function () {
   expandTextAreaOnPostArticlePage();
   handleDeleteFileOnPostArticlePage();
-})
+});
 
 function expandTextAreaOnPostArticlePage() {
-  var targetUrl = window.location.origin + '/posts/post/';
+  var targetUrl = window.location.origin + `/posts/post/`;
 
-  if(targetUrl.indexOf(window.location.href) >= 0) {
-    var textArea = getElById('textarea');
+  if (targetUrl.indexOf(window.location.href) >= 0) {
+    var textArea = getElById(`textarea`);
 
     for (var i = 0; i < textArea.length; i++) {
-      textArea[i].setAttribute('style', 'height:' + (textArea[i].scrollHeight) + 'px;overflow-y:hidden;');
-      textArea[i].addEventListener('input', onInput, false);
+      textArea[i].setAttribute(
+        `style`, `height:` + textArea[i].scrollHeight + `px;overflow-y:hidden;`,
+      );
+      textArea[i].addEventListener(`input`, onInput, false);
     }
   }
 }
 
 function handleDeleteFileOnPostArticlePage() {
-  if('/articles/article/edit/'.indexOf(window.location.pathname) >= 0) {
-    var articleFormImgDeleteBtn = getElById('image-file-delete');
+  if (`/articles/article/edit/`.indexOf(window.location.pathname) >= 0) {
+    var articleFormImgDeleteBtn = getElById(`image-file-delete`);
 
-    if(articleFormImgDeleteBtn) {
-      articleFormImgDeleteBtn.addEventListener('click', function(e) {
+    if (articleFormImgDeleteBtn) {
+      articleFormImgDeleteBtn.addEventListener(`click`, function (e) {
         e.preventDefault();
 
-        var articleFormImg = getElById('image-name-field');
-        var prevArticleFormImg = getElById('prevImage-name-field');
+        var articleFormImg = getElById(`image-name-field`);
+        var prevArticleFormImg = getElById(`prevImage-name-field`);
 
         prevArticleFormImg.value = articleFormImg.value;
-        articleFormImg.value = '';
-      })
+        articleFormImg.value = ``;
+      });
     }
   }
 }
 
-function onInput(e) {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
+function onInput() {
+  this.style.height = `auto`;
+  this.style.height = this.scrollHeight + `px`;
 }
 
 function getElById(id) {
