f015fae7ec98d39a6e47524e5b0edd41a46709b4
topus009 <sj_89@mail.ru>
2020-08-28 13:58:56 +0300

    fix addEventListener

--- src/frontend/public/js/fixes.js
+++ src/frontend/public/js/fixes.js
@@ -1,26 +1,31 @@
+function onInput(e) {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var location = window.location
+  var pathname = location.pathname
   var targetUrl = location.origin + '/posts/post/';
   if(~~targetUrl.indexOf(location.href)) {
     var tx = document.getElementsByTagName('textarea');
 
     for (var i = 0; i < tx.length; i++) {
       tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
-      tx[i].addEventListener("input", OnInput, false);
-    }
-
-    function OnInput(e) {
-      this.style.height = 'auto';
-      this.style.height = (this.scrollHeight) + 'px';
+      tx[i].addEventListener("input", onInput, false);
     }
   }
 
-  var articleFormImgDeleteBtn = document.getElementById('image-file-delete');
-  articleFormImgDeleteBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    var articleFormImg = document.getElementById('image-name-field');
-    var prevArticleFormImg = document.getElementById('prevImage-name-field');
-    prevArticleFormImg.value = articleFormImg.value;
-    articleFormImg.value = '';
-  })
+  if(~~'/articles/article/edit/'.indexOf(pathname)) {
+    var articleFormImgDeleteBtn = document.getElementById('image-file-delete');
+    if(articleFormImgDeleteBtn) {
+      articleFormImgDeleteBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        var articleFormImg = document.getElementById('image-name-field');
+        var prevArticleFormImg = document.getElementById('prevImage-name-field');
+        prevArticleFormImg.value = articleFormImg.value;
+        articleFormImg.value = '';
+      })
+    }
+  }
 })
