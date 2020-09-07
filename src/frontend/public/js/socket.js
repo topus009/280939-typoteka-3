d5f0d821e86986e718ddb9ae4849439dd56a027a
topus009 <sj_89@mail.ru>
2020-09-07 19:07:09 +0300

    TOR fixes - 01

--- src/frontend/public/js/socket.js
+++ src/frontend/public/js/socket.js
@@ -1,36 +1,38 @@
 'use strict';
 
-document.addEventListener(`DOMContentLoaded`, function () {
-  var socket = io();
+const COMMENTS_MAX_LATEST_COUNT = 4;
 
-  socket.on(`NEW_COMMENT`, function (data) {
+const updateLastComments = (data) => {
+  const lastHTML = data.lastHTML;
+  const lastList = document.getElementById(`last-list`);
+  const firstComment = lastList.childNodes[0];
+  const commentsLength = lastList.childNodes.length;
+
+  firstComment.insertAdjacentHTML(`beforebegin`, lastHTML);
+
+  if (commentsLength === COMMENTS_MAX_LATEST_COUNT) {
+    lastList.childNodes[commentsLength - 1].remove();
+  }
+};
+
+const updateHottestArticles = (data) => {
+  const hotHTML = data.hotHTML;
+  const hotList = document.getElementById(`hot-list`);
+  hotList.innerHTML = hotHTML;
+};
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  const socket = io();
+
+  socket.on(`NEW_COMMENT`, (data) => {
     updateLastComments(data);
     updateHottestArticles(data);
   });
 
-  socket.on(`MAIN_PAGE_READERS_COUNT`, function (data) {
-    var currentReaders = document.querySelectorAll(`.current-page-readers`);
+  socket.on(`MAIN_PAGE_READERS_COUNT`, (data) => {
+    const currentReaders = document.querySelectorAll(`.current-page-readers`);
     if (currentReaders.length) {
-      currentReaders[0].innerText = `Смотрят эту страницу: ` + data;
+      currentReaders[0].innerText = `Смотрят эту страницу: ${data}`;
     }
   });
 });
-
-function updateLastComments(data) {
-  var lastHTML = data.lastHTML;
-  var lastList = document.getElementById(`last-list`);
-  var firstComment = lastList.childNodes[0];
-  var commentsLength = lastList.childNodes.length;
-
-  firstComment.insertAdjacentHTML(`beforebegin`, lastHTML);
-
-  if (commentsLength === 4) {
-    lastList.childNodes[commentsLength - 1].remove();
-  }
-}
-
-function updateHottestArticles(data) {
-  var hotHTML = data.hotHTML;
-  var hotList = document.getElementById(`hot-list`);
-  hotList.innerHTML = hotHTML;
-}
